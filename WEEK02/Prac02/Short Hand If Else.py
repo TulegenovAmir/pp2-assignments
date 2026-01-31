585891c2ce55069a5365dc215c60fd3e4dcf4f5f
TulegenovAmir <amir.tulegenov19@gmail.com>
2026-01-31 22:16:10 +0500

    add week02 prac02 short hand If else examples

---
+++ WEEK02/Prac02/Short Hand If Else.py
@@ -0,0 +1,16 @@
+#1
+a = int(input())
+print("Yes" if a > 0 else "No")
+#2
+a = int(input())
+print("Even" if a % 2 == 0 else "Odd")
+#3
+a = int(input())
+b = int(input())
+print(a if a > b else b)
+#4
+a = int(input())
+print("Big" if a > 10 else "Small")
+#5
+a = int(input())
+print("OK" if a == 5 else "NO")
